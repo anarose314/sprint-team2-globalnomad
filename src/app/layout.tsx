9490a4264e1d242c1af81482795066ec119344a8
anarose314 <dpgp706@gmail.com>
2026-04-20 20:01:06 +0900

    🐛 Fix: 네이밍 규칙에 따른 export 방식 수정 (default -> named)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import QueryProvider from '@/shared/components/query-provider';
+import { QueryProvider } from '@/shared/components/query-provider';
 import '@/app/styles/globals.css';
 
 export const metadata: Metadata = {
