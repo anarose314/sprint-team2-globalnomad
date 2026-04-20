6f0b45dbba9d6eba0f7975956ebcdd3226fb451e
anarose314 <dpgp706@gmail.com>
2026-04-20 23:22:33 +0900

    📝 Docs: TSDoc 주석 추가

--- src/shared/components/heading/index.tsx
+++ src/shared/components/heading/index.tsx
@@ -1,6 +1,18 @@
 import { HeadingProps } from '@/shared/components/heading/heading.types';
 import { cn } from '@/shared/utils/cn';
 
+/**
+ * 전역 공통 타이포그래피 컴포넌트
+ *
+ * @example
+ * // 1. 기본 사용 (h2, typo-2lg-bold, text-gray-950)
+ * <Heading>제목</Heading>
+ *
+ * // 2. 태그 및 텍스트 스타일 변경
+ * <Heading as="h3" textStyle="typo-lg-bold" className="md:typo-2xl-bold text-center">
+ * 제목
+ * </Heading>
+ */
 export function Heading({
   as = 'h2',
   textStyle = 'typo-2lg-bold',
