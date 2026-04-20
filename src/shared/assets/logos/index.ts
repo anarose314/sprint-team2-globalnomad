efdc2ce954520e3d018b9d1fa3960dba90c390cb
anarose314 <dpgp706@gmail.com>
2026-04-20 10:05:07 +0900

    🔧 Chore: SVGR 환경 세팅 및 공통 아이콘 컴포넌트 시스템 구축

---
+++ src/shared/assets/logos/index.ts
@@ -0,0 +1,5 @@
+import LogoHorizontal from '@/shared/assets/icons/logo-horizontal.svg';
+import LogoIcon from '@/shared/assets/icons/logo-icon.svg';
+import LogoVertical from '@/shared/assets/icons/logo-vertical.svg';
+
+export { LogoHorizontal, LogoIcon, LogoVertical };
