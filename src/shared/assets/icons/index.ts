ad6ade990936c3dd115873e861a9d40b949aab61
anarose314 <66053969+anarose314@users.noreply.github.com>
2026-04-20 10:16:36 +0900

    [Feat/#13] svgr 및 이미지 에셋 세팅 (#18)

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,69 @@
+import IcArrowDown from '@/shared/assets/icons/ic-arrow-down.svg';
+import IcArrowLeft from '@/shared/assets/icons/ic-arrow-left.svg';
+import IcArrowNaviLeft from '@/shared/assets/icons/ic-arrow-navi-left.svg';
+import IcArrowNaviRight from '@/shared/assets/icons/ic-arrow-navi-right.svg';
+import IcArrowRight from '@/shared/assets/icons/ic-arrow-right.svg';
+import IcArrowUp from '@/shared/assets/icons/ic-arrow-up.svg';
+import IcArt from '@/shared/assets/icons/ic-art.svg';
+import IcBack from '@/shared/assets/icons/ic-back.svg';
+import IcBellOff from '@/shared/assets/icons/ic-bell-off.svg';
+import IcBellOn from '@/shared/assets/icons/ic-bell-on.svg';
+import IcBus from '@/shared/assets/icons/ic-bus.svg';
+import IcCalendar from '@/shared/assets/icons/ic-calendar.svg';
+import IcClose from '@/shared/assets/icons/ic-close.svg';
+import IcEdit from '@/shared/assets/icons/ic-edit.svg';
+import IcEmpty from '@/shared/assets/icons/ic-empty.svg';
+import IcEyeOff from '@/shared/assets/icons/ic-eye-off.svg';
+import IcEyeOn from '@/shared/assets/icons/ic-eye-on.svg';
+import IcFood from '@/shared/assets/icons/ic-food.svg';
+import IcGithub from '@/shared/assets/icons/ic-github.svg';
+import IcKakao from '@/shared/assets/icons/ic-kakao.svg';
+import IcList from '@/shared/assets/icons/ic-list.svg';
+import IcMap from '@/shared/assets/icons/ic-map.svg';
+import IcMinus from '@/shared/assets/icons/ic-minus.svg';
+import IcMore from '@/shared/assets/icons/ic-more.svg';
+import IcPlus from '@/shared/assets/icons/ic-plus.svg';
+import IcProfile from '@/shared/assets/icons/ic-profile.svg';
+import IcSearch from '@/shared/assets/icons/ic-search.svg';
+import IcSetting from '@/shared/assets/icons/ic-setting.svg';
+import IcSport from '@/shared/assets/icons/ic-sport.svg';
+import IcStar from '@/shared/assets/icons/ic-star.svg';
+import IcTour from '@/shared/assets/icons/ic-tour.svg';
+import IcUser from '@/shared/assets/icons/ic-user.svg';
+import IcWellbeing from '@/shared/assets/icons/ic-wellbeing.svg';
+
+export {
+  IcArrowDown,
+  IcArrowLeft,
+  IcArrowNaviLeft,
+  IcArrowNaviRight,
+  IcArrowRight,
+  IcArrowUp,
+  IcArt,
+  IcBack,
+  IcBellOff,
+  IcBellOn,
+  IcBus,
+  IcCalendar,
+  IcClose,
+  IcEdit,
+  IcEmpty,
+  IcEyeOff,
+  IcEyeOn,
+  IcFood,
+  IcGithub,
+  IcKakao,
+  IcList,
+  IcMap,
+  IcMinus,
+  IcMore,
+  IcPlus,
+  IcProfile,
+  IcSearch,
+  IcSetting,
+  IcSport,
+  IcStar,
+  IcTour,
+  IcUser,
+  IcWellbeing,
+};
