fbaef5c73e10eb6a2297af3cab682c55f76ed905
anarose314 <dpgp706@gmail.com>
2026-04-24 12:04:46 +0900

    ✨ Feat: 메인 레이아웃 추가

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,23 @@
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {/* TODO: 헤더 작업 */}
+      <header className="bg-primary-50 flex h-20 shrink-0 items-center px-4">
+        Header
+      </header>
+      <div className="flex w-full grow px-6 md:px-7.5">
+        <main className="mx-auto flex w-full max-w-280 flex-col">
+          {children}
+        </main>
+      </div>
+      {/* TODO: 푸터 작업 */}
+      <footer className="bg-primary-50 flex h-20 shrink-0 items-center px-4">
+        Footer
+      </footer>
+    </>
+  );
+}
