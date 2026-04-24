a8af7bbabdea53eeda2e90786da1be42ab22a93b
SeungmiLee <yyaapple@gmail.com>
2026-04-24 19:01:49 +0900

    [feat/#49] ModalBase UI 구현 및 index.tsx 파일 작업

---
+++ src/shared/components/modal/index.tsx
@@ -0,0 +1,5 @@
+export { ModalBase } from '@/shared/components/modal/ModalBase';
+export { OneButtonModal } from '@/shared/components/modal/OneButtonModal';
+export { TwoButtonModal } from '@/shared/components/modal/TwoButtonModal';
+export { ReviewModal } from '@/shared/components/modal/ReviewModal';
+export { NotificationModal } from '@/shared/components/modal/NotificationModal';
