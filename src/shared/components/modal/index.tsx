8d72f07b7a674cd5902a124dc364f2258c9ab874
SeungmiLee <yyaapple@gmail.com>
2026-04-24 19:22:21 +0900

    fix: 존재하지 않는 모달 export 제거

--- src/shared/components/modal/index.tsx
+++ src/shared/components/modal/index.tsx
@@ -1,5 +1,1 @@
 export { ModalBase } from '@/shared/components/modal/ModalBase';
-export { OneButtonModal } from '@/shared/components/modal/OneButtonModal';
-export { TwoButtonModal } from '@/shared/components/modal/TwoButtonModal';
-export { ReviewModal } from '@/shared/components/modal/ReviewModal';
-export { NotificationModal } from '@/shared/components/modal/NotificationModal';
