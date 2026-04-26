b14df22aa4f7a9afd0759f0536dd1179f5eb5087
SeungmiLee <yyaapple@gmail.com>
2026-04-27 02:49:39 +0900

    📝 Docs: TwoButtonModal TSDoc 주석 추가

--- src/shared/components/modal/TwoButtonModal.tsx
+++ src/shared/components/modal/TwoButtonModal.tsx
@@ -1,3 +1,19 @@
+/**
+ * 두 개의 액션 버튼을 제공하는 공통 모달 컴포넌트입니다.
+ *
+ * - 취소/확인처럼 사용자가 선택지를 결정해야 하는 상황에 사용합니다.
+ * - ModalBase를 기반으로 하며, 버튼 텍스트와 클릭 핸들러를 props로 전달받습니다.
+ *
+ * @example
+ * <TwoButtonModal
+ *   title="예약을 취소하시겠어요?"
+ *   cancelText="아니오"
+ *   confirmText="네"
+ *   onCancel={handleClose}
+ *   onConfirm={handleConfirm}
+ * />
+ */
+
 import { ModalBase } from '@/shared/components/modal/ModalBase';
 
 interface TwoButtonModalProps {
