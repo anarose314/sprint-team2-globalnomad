636f7c0e45fae259f44dab821ac89b336e535f5d
dltmdall <107185525+dltmdall@users.noreply.github.com>
2026-04-27 22:40:31 +0900

    [Feat/#52] TwoButtonModal UI 구현 (#91)
    Co-authored-by: SeungmiLee <yyaapple@gmail.com>

--- src/shared/components/modal/index.tsx
+++ src/shared/components/modal/index.tsx
@@ -1,2 +1,3 @@
 export { ModalBase } from '@/shared/components/modal/ModalBase';
 export { OneButtonModal } from '@/shared/components/modal/OneButtonModal';
+export { TwoButtonModal } from '@/shared/components/modal/TwoButtonModal';
