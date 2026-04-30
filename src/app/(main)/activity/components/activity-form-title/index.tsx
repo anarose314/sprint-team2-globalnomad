2db72caeef90a1ca46f9595292d308a94ed6fbcd
anarose314 <dpgp706@gmail.com>
2026-04-30 10:10:48 +0900

    ♻️ Refactor: Heading 컴포넌트에 일부 클래스는 textStyle prop으로 변경

--- src/app/(main)/activity/components/activity-form-title/index.tsx
+++ src/app/(main)/activity/components/activity-form-title/index.tsx
@@ -2,7 +2,7 @@ import { Heading } from '@/shared/components/heading';
 
 export function ActivityFormTitle({ children }: { children: React.ReactNode }) {
   return (
-    <Heading as="h3" className="typo-lg-bold mb-4.5">
+    <Heading as="h3" textStyle="typo-lg-bold" className="mb-4.5">
       {children}
     </Heading>
   );
