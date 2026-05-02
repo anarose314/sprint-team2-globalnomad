3a5c93a95dcd33456c700e99e894ce43eac50452
anarose314 <dpgp706@gmail.com>
2026-05-02 12:15:15 +0900

    🔧 Chore: NEXT_PUBLIC_IMAGE_URL 환경 변수 next.config.ts 에 반영

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 import type { NextConfig } from 'next';
 
+const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
+if (!imageUrl) {
+  throw new Error(
+    '🚨 NEXT_PUBLIC_IMAGE_URL 환경 변수가 누락되었습니다. .env 파일을 확인해주세요!'
+  );
+}
+const imageHostname = new URL(imageUrl).hostname;
+
 const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
@@ -17,6 +25,15 @@ const nextConfig: NextConfig = {
         as: '*.js',
       },
     },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: imageHostname,
+      },
+    ],
   },
 };
 
