63978a15c49dc7774e29ced1a793eaf9f311abda
anarose314 <dpgp706@gmail.com>
2026-05-02 12:18:55 +0900

    ✨ Feat: 마이페이지 예약 내역(ReserveList) 섹션 UI 구현

--- src/app/(main)/my/reservations/components/reserve-list/index.tsx
+++ src/app/(main)/my/reservations/components/reserve-list/index.tsx
@@ -1,3 +1,64 @@
+import Image from 'next/image';
+import { ActivityCard } from '@/app/(main)/my/components/activity-card';
+import { ReserveButtons } from '@/app/(main)/my/reservations/components/reserve-buttons';
+import { DUMMY_RESERVATION_LIST } from '@/app/(main)/my/reservations/components/reserve-list/reserveList.constants';
+import { Heading } from '@/shared/components/heading';
+import { StatusBadge } from '@/shared/components/status-badge';
+import { ReservationStatus } from '@/shared/constants/status.constants';
+
 export function ReserveList() {
-  return <>List</>;
+  // TODO: API 데이터 연동
+  const reserveList = DUMMY_RESERVATION_LIST.reservations;
+
+  return (
+    <section className="mt-7.5">
+      <ul className="flex flex-col gap-5 wrap-anywhere">
+        {reserveList.map((list) => (
+          <li key={list.id}>
+            <article className="flex flex-col gap-3 border-b border-b-gray-50 pb-7.5">
+              {/* 날짜 */}
+              <time
+                dateTime={list.date}
+                className="typo-lg-bold 2xl:typo-2lg-bold block"
+              >
+                {list.date}
+              </time>
+              {/* 카드 */}
+              {/* TODO: API 데이터 연동하면 해당 체험 url 넣기 */}
+              <ActivityCard href="/">
+                <div className="flex flex-1 flex-col justify-center gap-2 px-4 py-4">
+                  <StatusBadge status={list.status as ReservationStatus} />
+                  <div>
+                    <Heading as="h3" className="typo-md-bold 2xl:typo-2lg-bold">
+                      {list.activity.title}
+                    </Heading>
+                    <p className="typo-sm-medium 2xl:typo-lg-medium text-gray-500">
+                      <time dateTime={list.startTime}>{list.startTime}</time> -{' '}
+                      <time dateTime={list.endTime}>{list.endTime}</time>
+                    </p>
+                  </div>
+                  <p className="typo-lg-bold 2xl:typo-2lg-bold flex items-center gap-1 text-gray-950">
+                    ₩{list.totalPrice.toLocaleString('ko-KR')}
+                    <span className="typo-md-medium 2xl:typo-lg-medium text-gray-400">
+                      {list.headCount}명
+                    </span>
+                  </p>
+                </div>
+                <figure className="relative w-1/3 shrink-0 overflow-hidden md:w-1/4">
+                  <Image
+                    fill
+                    src={list.activity.bannerImageUrl}
+                    alt={list.activity.title}
+                    className="object-cover transition-transform duration-300 group-hover:scale-105"
+                  />
+                </figure>
+              </ActivityCard>
+              {/* 버튼 */}
+              <ReserveButtons status={list.status as ReservationStatus} />
+            </article>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
