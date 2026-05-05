f38f815a9d34f5e5af3bfed8f857e3bb98ac7b9f
anarose314 <dpgp706@gmail.com>
2026-05-05 17:41:45 +0900

    ✨ Feat: 내 체험 수정 페이지 퍼블리싱

--- src/app/(main)/activity/[id]/edit/page.tsx
+++ src/app/(main)/activity/[id]/edit/page.tsx
@@ -1,4 +1,6 @@
 import { Metadata } from 'next';
+import { ActivityEditForm } from '@/app/(main)/activity/[id]/edit/components/activity-edit-form';
+import { Heading } from '@/shared/components/heading';
 
 // TODO: 추후 API 연동 후 동적 이름으로 변경
 export const metadata: Metadata = {
@@ -6,5 +8,10 @@ export const metadata: Metadata = {
 };
 
 export default function ActivityEditPage() {
-  return <>ActivityEditPage</>;
+  return (
+    <div className="mx-auto mt-7.5 mb-9 max-w-175 md:mt-10 md:mb-16 2xl:mb-30">
+      <Heading>내 체험 수정</Heading>
+      <ActivityEditForm />
+    </div>
+  );
 }
