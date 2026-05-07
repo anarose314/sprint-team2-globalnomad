2fe7d1a9c7e966d956ed1a874ee007d4523a5fa5
anarose314 <dpgp706@gmail.com>
2026-05-07 16:34:06 +0900

    ✨ Feat: 체험 등록하기 버튼 링크 연결

--- src/app/(main)/my/activities/page.tsx
+++ src/app/(main)/my/activities/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
+import { ActivityButton } from '@/app/(main)/my/activities/components/activityAddButton';
 import { MyActivitiesList } from '@/app/(main)/my/activities/components/my-activities-list';
 import { MyPageHeader } from '@/app/(main)/my/components/my-page-header';
-import { Button } from '@/shared/components/buttons';
 
 export const metadata: Metadata = {
   title: '내 체험 관리',
@@ -14,7 +14,7 @@ export default function MyActivitiesPage() {
         title="내 체험 관리"
         description="체험을 등록하거나 수정 및 삭제가 가능합니다."
       >
-        <Button className="w-full">체험 등록하기</Button>
+        <ActivityButton />
       </MyPageHeader>
       <MyActivitiesList />
     </section>
