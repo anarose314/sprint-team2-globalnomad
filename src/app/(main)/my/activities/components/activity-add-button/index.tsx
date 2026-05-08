2ce056c06bef9e8949a568159c5bb94114c5f87a
anarose314 <dpgp706@gmail.com>
2026-05-08 18:07:11 +0900

    ♻️ Refactor: HydrationBoundary 범위 확장 및 버튼 조건부 렌더링 적용

--- src/app/(main)/my/activities/components/activity-add-button/index.tsx
+++ src/app/(main)/my/activities/components/activity-add-button/index.tsx
@@ -1,9 +1,14 @@
 'use client';
 
 import Link from 'next/link';
+import { useMyActivitiesInfinite } from '@/app/(main)/my/activities/hooks/useMyActivitiesInfinite';
 import { Button } from '@/shared/components/buttons';
 
-export function ActivityButton() {
+export function ActivityAddButton() {
+  const { data } = useMyActivitiesInfinite();
+  const totalCount = data?.pages[0]?.totalCount ?? 0;
+
+  if (totalCount === 0) return null;
   return (
     <Button as={Link} href="/activity/add" className="w-full">
       체험 등록하기
--- src/app/(main)/my/activities/page.tsx
+++ src/app/(main)/my/activities/page.tsx
@@ -5,7 +5,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query';
 import { MY_ACTIVITIES_SIZE } from '@/app/(main)/my/activities/activities.constants';
-import { ActivityButton } from '@/app/(main)/my/activities/components/activity-add-button';
+import { ActivityAddButton } from '@/app/(main)/my/activities/components/activity-add-button';
 import { MyActivitiesList } from '@/app/(main)/my/activities/components/my-activities-list';
 import { myActivitiesOptions } from '@/app/(main)/my/activities/hooks/useMyActivitiesInfinite';
 import { MyPageHeader } from '@/app/(main)/my/components/my-page-header';
@@ -29,17 +29,17 @@ export default async function MyActivitiesPage() {
 
   return (
     <section className="pb-16 2xl:pb-40">
-      <MyPageHeader
-        title="내 체험 관리"
-        description="체험을 등록하거나 수정 및 삭제가 가능합니다."
-      >
-        <ActivityButton />
-      </MyPageHeader>
-      <section className="mt-7.5">
-        <HydrationBoundary state={dehydrate(queryClient)}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <MyPageHeader
+          title="내 체험 관리"
+          description="체험을 등록하거나 수정 및 삭제가 가능합니다."
+        >
+          <ActivityAddButton />
+        </MyPageHeader>
+        <section className="mt-7.5">
           <MyActivitiesList />
-        </HydrationBoundary>
-      </section>
+        </section>
+      </HydrationBoundary>
     </section>
   );
 }
