16d99e25e0f759d59995d27a8b1c6a9f23d9f23c
SeungmiLee <yyaapple@gmail.com>
2026-05-08 19:10:51 +0900

    📝 Docs: MainContent TSDoc 주석 형식 수정

--- src/app/(main)/components/main-content/index.tsx
+++ src/app/(main)/components/main-content/index.tsx
@@ -5,7 +5,6 @@ import { PopularActivitySection } from '@/app/(main)/components/popular-activity
 import { POPULAR_ACTIVITIES } from '@/app/(main)/main.constants';
 
 /**
-/**
  * 메인 페이지 콘텐츠 조립 컴포넌트
  *
  * - 배너, 검색, 인기 체험, 모든 체험 섹션을 순서대로 조립한다.
