03d4fcb6fd27b25b5bc99e677ffdc7802aea4fa1
anarose314 <66053969+anarose314@users.noreply.github.com>
2026-05-09 14:56:34 +0900

    [Feat/#134] 예약 내역 API 연동 및 리팩토링 (#154)

--- src/app/(main)/my/reservations/components/reserve-container/index.tsx
+++ src/app/(main)/my/reservations/components/reserve-container/index.tsx
@@ -1,11 +1,34 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import { ReserveFilter } from '@/app/(main)/my/reservations/components/reserve-filter';
 import { ReserveList } from '@/app/(main)/my/reservations/components/reserve-list';
+import { myReservationsOptions } from '@/app/(main)/my/reservations/hooks/useMyReservations';
+import { MY_RESERVATIONS_SIZE } from '@/app/(main)/my/reservations/reservations.constants';
+import { fetchInstanceServer } from '@/shared/apis/fetchInstance.server';
+import { MyReservationsResponse } from '@/shared/types/myReservation.types';
 
-export function ReserveContainer() {
+export async function ReserveContainer() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    ...myReservationsOptions(),
+    queryFn: () =>
+      fetchInstanceServer<MyReservationsResponse>(
+        `/my-reservations?size=${MY_RESERVATIONS_SIZE}`
+      ),
+  });
+
   return (
     <>
       <ReserveFilter />
-      <ReserveList />
+      <section className="mt-7.5">
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <ReserveList />
+        </HydrationBoundary>
+      </section>
     </>
   );
 }
---
+++ src/app/(main)/my/reservations/reservations.constants.ts
@@ -0,0 +1,1 @@
+export const MY_RESERVATIONS_SIZE = 5;
