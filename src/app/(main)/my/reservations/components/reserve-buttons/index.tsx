a3f01a39ad652f55d5979582328c672e981c9382
anarose314 <dpgp706@gmail.com>
2026-05-13 23:22:55 +0900

    ✨ Feat: 예약 취소 기능 추가

--- src/app/(main)/my/reservations/components/reserve-buttons/index.tsx
+++ src/app/(main)/my/reservations/components/reserve-buttons/index.tsx
@@ -1,31 +1,18 @@
 'use client';
 
 import { ReserveButtonsProps } from '@/app/(main)/my/reservations/components/reserve-buttons/reserveButtons.types';
+import { ReserveCancel } from '@/app/(main)/my/reservations/components/reserve-cancel';
 import { ReserveReview } from '@/app/(main)/my/reservations/components/reserve-review';
-import { Button } from '@/shared/components/buttons';
 
 export function ReserveButtons({ reservationInfo }: ReserveButtonsProps) {
-  // TODO: 예약 취소 이벤트 작성
-  const handleCancel = () => {
-    console.log('예약 취소 이벤트');
-  };
+  const { status, id, reviewSubmitted } = reservationInfo;
 
   return (
     <>
-      {reservationInfo.status === 'pending' && (
-        <Button
-          variant="secondary"
-          size="md"
-          className="w-full"
-          onClick={handleCancel}
-        >
-          예약 취소
-        </Button>
+      {status === 'pending' && <ReserveCancel reservationId={id} />}
+      {status === 'completed' && !reviewSubmitted && (
+        <ReserveReview reservationInfo={reservationInfo} />
       )}
-      {reservationInfo.status === 'completed' &&
-        !reservationInfo.reviewSubmitted && (
-          <ReserveReview reservationInfo={reservationInfo} />
-        )}
     </>
   );
 }
