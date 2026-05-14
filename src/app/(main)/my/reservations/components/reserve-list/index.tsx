35add63c333cae71715427473f6fb9fe6d8553e9
anarose314 <dpgp706@gmail.com>
2026-05-14 23:01:21 +0900

    ♻️ Refactor: 예약 내역 및 내 체험 관리 페이지에 적용

--- src/app/(main)/my/reservations/components/reserve-list/index.tsx
+++ src/app/(main)/my/reservations/components/reserve-list/index.tsx
@@ -1,15 +1,12 @@
 'use client';
 
 import { useInView } from 'react-intersection-observer';
-import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { ActivityCard } from '@/app/(main)/my/components/activity-card';
 import { MyPageEmpty } from '@/app/(main)/my/components/my-page-empty';
 import { ReserveButtons } from '@/app/(main)/my/reservations/components/reserve-buttons';
 import { useMyReservations } from '@/app/(main)/my/reservations/hooks/useMyReservations';
-import { Heading } from '@/shared/components/heading';
 import { Spinner } from '@/shared/components/spinner';
-import { StatusBadge } from '@/shared/components/status-badge';
 
 export function ReserveList() {
   const searchParams = useSearchParams();
@@ -38,7 +35,7 @@ export function ReserveList() {
           href="/"
         />
       ) : (
-        <ul className="flex flex-col gap-5 wrap-anywhere">
+        <ul className="flex flex-col gap-7.5 wrap-anywhere">
           {reserveList.map((reservation, index) => {
             const {
               id,
@@ -61,60 +58,29 @@ export function ReserveList() {
             };
 
             return (
-              <li key={id}>
-                <article className="flex flex-col gap-3 border-b border-b-gray-50 pb-7.5">
-                  {/* 카드 영역 */}
-                  <ActivityCard href={`/activity/${activity.id}`}>
-                    <div className="flex flex-1 flex-col justify-center gap-2 px-4 py-4">
-                      {/* 뱃지 */}
-                      <StatusBadge status={status} />
-                      <div>
-                        {/* 제목 */}
-                        <Heading
-                          as="h3"
-                          className="typo-md-bold 2xl:typo-2lg-bold"
-                        >
-                          {activity.title}
-                        </Heading>
-                        {/* 날짜 · 시간 */}
-                        <div className="typo-sm-medium 2xl:typo-lg-medium flex gap-2 text-gray-500">
-                          {/* 날짜 */}
-                          <span>
-                            <time dateTime={date}>{date}</time>
-                          </span>
-                          {/* 구분자 */}
-                          <span aria-hidden="true">·</span>
-                          {/* 시간 */}
-                          <span>
-                            <time dateTime={startTime}>{startTime}</time> -{' '}
-                            <time dateTime={endTime}>{endTime}</time>
-                          </span>
-                        </div>
-                      </div>
-                      {/* 가격 및 인원 */}
-                      <p className="typo-lg-bold 2xl:typo-2lg-bold flex items-center gap-1 text-gray-950">
-                        ₩{totalPrice.toLocaleString('ko-KR')}
-                        <span className="typo-md-medium 2xl:typo-lg-medium text-gray-400">
-                          {headCount}명
-                        </span>
-                      </p>
-                    </div>
-                    {/* 이미지 영역 */}
-                    <figure className="relative w-1/3 shrink-0 overflow-hidden md:w-1/4">
-                      <Image
-                        fill
-                        src={activity.bannerImageUrl}
-                        alt={activity.title}
-                        priority={index === 0}
-                        sizes="(min-width: 768px) 25vw, 33vw"
-                        className="object-cover transition-transform duration-300 group-hover:scale-105"
-                      />
-                    </figure>
-                  </ActivityCard>
-                  {/* 하단 버튼 영역 */}
+              <ActivityCard key={id}>
+                <ActivityCard.Info
+                  index={index}
+                  title={activity.title}
+                  activityId={activity.id}
+                  bannerImageUrl={activity.bannerImageUrl}
+                >
+                  <ActivityCard.Status status={status} />
+                  <ActivityCard.Heading title={activity.title} />
+                  <ActivityCard.DateTime
+                    date={date}
+                    startTime={startTime}
+                    endTime={endTime}
+                  />
+                  <ActivityCard.Price
+                    price={totalPrice}
+                    headCount={headCount}
+                  />
+                </ActivityCard.Info>
+                <ActivityCard.Buttons>
                   <ReserveButtons reservationInfo={reservationInfo} />
-                </article>
-              </li>
+                </ActivityCard.Buttons>
+              </ActivityCard>
             );
           })}
         </ul>
