cde8620d099d2ae7d13d4aa253e4d2fe565ec209
anarose314 <dpgp706@gmail.com>
2026-05-15 10:01:44 +0900

    🐛 Fix: 불필요한 article 삭제

--- src/app/(main)/my/components/my-activity-card-skeleton/index.tsx
+++ src/app/(main)/my/components/my-activity-card-skeleton/index.tsx
@@ -6,12 +6,10 @@ export function MyActivityCardSkeleton() {
     <ul className="flex flex-col gap-7.5 wrap-anywhere">
       {Array.from({ length: 3 }).map((_, i) => (
         <MyActivityCard key={i}>
-          <article className="flex flex-col gap-3">
-            {/* 카드 영역 */}
-            <Skeleton className="min-h-45 w-full rounded-3xl" />
-            {/* 하단 버튼 영역 */}
-            <Skeleton fullWidth height={48} rounded="lg" />
-          </article>
+          {/* 카드 영역 */}
+          <Skeleton className="min-h-45 w-full rounded-3xl" />
+          {/* 하단 버튼 영역 */}
+          <Skeleton fullWidth height={48} rounded="lg" />
         </MyActivityCard>
       ))}
     </ul>
