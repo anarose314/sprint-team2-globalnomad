a32389de3d534af6b71d0c37d14b408860cb1c9c
anarose314 <66053969+anarose314@users.noreply.github.com>
2026-05-16 22:23:02 +0900

    [Fix/#216] 체험 등록/수정 시 지난 시간 등록 방지 (#244)

--- src/app/(main)/activity/add/components/activity-add-form/index.tsx
+++ src/app/(main)/activity/add/components/activity-add-form/index.tsx
@@ -4,12 +4,27 @@ import { PostActivities } from '@/app/(main)/activity/apis/activities.types';
 import { ActivityForm } from '@/app/(main)/activity/components/activity-form';
 import { ActivityFormValues } from '@/app/(main)/activity/components/activity-form/activityForm.schema';
 import { usePostActivities } from '@/app/(main)/activity/hooks/usePostActivities';
+import { isValidReserveTime } from '@/app/(main)/activity/utils/isValidReserveTime';
 import { Button } from '@/shared/components/buttons';
+import { useShowToast } from '@/shared/store/useToastStore';
 
 export function ActivityAddForm() {
   const { mutate: createActivity, isPending } = usePostActivities();
+  const showToast = useShowToast();
 
   const handleAddSubmit = (data: ActivityFormValues) => {
+    const hasInvalidSchedule = data.schedules.some(
+      (schedule) => !isValidReserveTime(schedule.date, schedule.startTime)
+    );
+
+    if (hasInvalidSchedule) {
+      showToast({
+        theme: 'error',
+        message: '현재 시간으로부터 1시간 이후의 일정만 등록할 수 있습니다.',
+      });
+      return;
+    }
+
     const payload: PostActivities = {
       ...data,
       schedules: data.schedules.map(({ id: _id, ...rest }) => rest),
