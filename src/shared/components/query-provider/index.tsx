f38cbc699842a08ac7a61f1e3782bd81c82adb38
양하늘 <skygksmf5810@gmail.com>
2026-06-08 17:14:38 +0900

    ♻️ Refactor: dynamic 고정 선언 제거

--- src/shared/components/query-provider/index.tsx
+++ src/shared/components/query-provider/index.tsx
@@ -4,13 +4,16 @@ import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const ReactQueryDevtools = dynamic(
-  () =>
-    import('@tanstack/react-query-devtools').then(
-      (mod) => mod.ReactQueryDevtools
-    ),
-  { ssr: false }
-);
+const ReactQueryDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null
+    : dynamic(
+        () =>
+          import('@tanstack/react-query-devtools').then(
+            (mod) => mod.ReactQueryDevtools
+          ),
+        { ssr: false }
+      );
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const isDevelopment = process.env.NODE_ENV === 'development';
